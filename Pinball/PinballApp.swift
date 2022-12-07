407e43f87b32555d29c963e20972a646af9dbd6b
Charisma Kausar <charisma.kausar@gmail.com>
2022-12-08 01:35:06 +0300

    Initial Commit

---
+++ Pinball/PinballApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinballApp.swift
+//  Pinball
+//
+//  Created by Charisma Kausar on 8/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PinballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
